7ac84b8a7e00615a3e42e5881f20c2a471a75638
Federico Oldani <federico.oldani@linksfoundation.com>
2024-02-22 16:55:52 +0100

    change prod ckan image and remove sudo command

--- ckan/start_ckan.sh
+++ ckan/start_ckan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run the prerun script to init CKAN and create the default admin user
-sudo -u ckan -EH python3 prerun.py
+ckan -EH python3 prerun.py
 
 # Run any startup scripts provided by images extending this one
 if [[ -d "/docker-entrypoint.d" ]]
@@ -21,7 +21,7 @@ then
     # Start supervisord
     supervisord --configuration /etc/supervisord.conf &
     # Start uwsgi
-    sudo -u ckan -EH uwsgi uwsgi.ini
+    ckan -EH uwsgi uwsgi.ini
 else
   echo "[prerun] failed...not starting CKAN."
 fi
